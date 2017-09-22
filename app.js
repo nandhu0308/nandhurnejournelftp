fad3da96b3f83e9009fccaa0d6eab8d36d84b5a5
ramk_1011 <ram@limitlesscircle.com>
2017-09-22 12:02:09 +0530

    commit 22/9/2017 [2]

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 // const l_url='http://localhost:3000/';
 // const s_url='http://seller.haappyapp.com:8080/';
 // const api_url=s_url;
-exports.handler = function (event, context,callback) {
+exports.handler = function (event, context, callback) {
 
     // This example code only throws error. 
     var error = new Error("something is wrong");
     callback(error);
-   
-    var eventKey= event.Records[0].s3.object.key;
+
+    var eventKey = event.Records[0].s3.object.key;
     console.log(eventKey);
-     //var eventKey  = 'ka-mob-prajaa-pj-manju1_2017-09-21-14.18.08.889-UTC_0.mp4'
+    //var eventKey  = 'ka-mob-prajaa-pj-manju1_2017-09-21-14.18.08.889-UTC_0.mp4'
     var mainKey = eventKey.split('/')[1];
     console.log(mainKey);
     var underscoreBreaker = mainKey.split('_');
@@ -25,12 +25,12 @@ exports.handler = function (event, context,callback) {
     var stream_name = hyphenBreaker[3] + '-' + hyphenBreaker[4];
     console.log('applnname : ' + appln_name);
     console.log('stream_name : ' + stream_name);
- 
+
     var bucketParams = {
         Bucket: 'haappyapp-j/myFiles',
         Key: mainKey
     };
-    
+
     unirest.get('http://seller.haappyapp.com:8080/journal/settings/' + appln_name + '/' + stream_name)
         .headers({
             'Accept': 'application/json',
